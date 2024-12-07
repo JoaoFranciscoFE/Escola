6a58db8d23963214479e0a7fcc0fad4c77bfdc61
JoaoFranciscoFE <161509418+JoaoFranciscoFE@users.noreply.github.com>
2024-12-07 18:49:08 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/br/com/escola/estrelaguia/Main/Main.java

---
+++ src/br/com/escola/estrelaguia/Model/DisciplinaEletiva.java
@@ -0,0 +1,37 @@
+package br.com.escola.estrelaguia.Model;
+
+import br.com.escola.estrelaguia.Enums.TipoDisciplina;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisciplinaEletiva extends Disciplina {
+    private boolean requerAprovacao;
+    private String areaDeEstudo;
+    private int duracaoSemanas;
+    private String recursosExtras;
+
+    public DisciplinaEletiva(String nome, boolean requerAprovacao, String areaDeEstudo, int duracaoSemanas, String recursosExtras) {
+        super(nome, TipoDisciplina.ELETIVA, 0, "N/A", "30");
+        this.requerAprovacao = requerAprovacao;
+        this.areaDeEstudo = areaDeEstudo;
+        this.duracaoSemanas = duracaoSemanas;
+        this.recursosExtras = recursosExtras;
+    }
+
+
+    public int vagasRestantes() {
+        return getMaximoAlunos() - getAlunosMatriculados().size();
+    }
+
+
+    @Override
+    public void detalhesDisciplina() {
+        System.out.println("Disciplina Eletiva: " + getNome());
+        System.out.println("Requer Aprovação: " + (requerAprovacao ? "Sim" : "Não"));
+        System.out.println("Área de Estudo: " + areaDeEstudo);
+        System.out.println("Duração: " + duracaoSemanas + " semanas");
+        System.out.println("Recursos Extras: " + recursosExtras);
+        System.out.println("Vagas Restantes: " + vagasRestantes());
+    }
+}
